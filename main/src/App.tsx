4bda64a2db6765967fd1672c923fb54411e5bcaa
GitG0 <51382091+GitG0@users.noreply.github.com>
2023-06-06 10:38:28 -0400

    init

---
+++ main/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Home from './pages/Home.jsx'
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
---
+++ main/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import MyButton from "../components/MyButton";
+
+export default function Home(){
+    return (
+        <>
+        <div>
+            <h1>-Welcome to my homepage-</h1>
+            <MyButton></MyButton>
+        </div>
+        </>
+    );
+}
